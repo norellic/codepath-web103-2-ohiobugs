e070c94836c77bf6472517628e10b3328ab557e0
ongarrick <ong3@duke.edu>
2025-09-25 22:30:54 -0400

    finished template

---
+++ server/data/gifts.js
@@ -0,0 +1,54 @@
+const giftData = [
+    {
+        "id": 1,
+        "name": "Disco Ball Candle",
+        "pricePoint": "$",
+        "audience": "Candle Lovers",
+        "image":"https://tinyurl.com/39xc9h6y",
+        "description":"A party in a candle",
+        "submittedBy":"Sasha",
+        "submittedOn":"2022-09-04T14:48:00"
+    },
+    {
+        "id": 2,
+        "name": "Lego Flower Bouquet Kit",
+        "pricePoint": "$",
+        "audience": "Green Thumbs",
+        "image":"https://tinyurl.com/ydpv2bt9",
+        "description":"A gift and fun activity to do together",
+        "submittedBy":"Isabel",
+        "submittedOn":"2023-02-14T12:32:00"
+    },
+    {
+        "id": 3,
+        "name": "Nintendo Switch Lite",
+        "pricePoint": "$$",
+        "audience": "Gamers",
+        "image":"https://tinyurl.com/2cdfe59x",
+        "description":"A gift for gamers on-the-go",
+        "submittedBy":"Mario",
+        "submittedOn":"2022-09-13T11:01:00"
+    },
+    {
+        "id": 4,
+        "name": "Nike Panda Dunks",
+        "pricePoint": "$$",
+        "audience": "Sneakerheads",
+        "image":"https://tinyurl.com/y9bzv8xd",
+        "description":"A fan-favorite black and white color-blocking sneaker",
+        "submittedBy":"Jason",
+        "submittedOn":"2023-03-10T05:23:00"
+    },
+    {
+        "id": 5,
+        "name": "Razer Kitty Headset",
+        "pricePoint": "$$",
+        "audience": "Gamers",
+        "image":"https://tinyurl.com/4pt84wry",
+        "description":"Create the purrfect gaming look with your own killer kitty style",
+        "submittedBy":"Pixie",
+        "submittedOn":"2011-10-10T14:48:00"
+    }
+]
+
+export default giftData
